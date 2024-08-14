b6abebd2e789d666f24cd2280833ff4a9638c416
olikr3 <oliver.kremser@gmail.com>
2024-08-14 17:54:41 +0200

    Add files via upload

---
+++ src/board/board.rs
@@ -0,0 +1,71 @@
+
+#[derive(Debug, Clone, Copy, Hash)]
+pub struct BB (u64);
+
+impl BB {
+
+    fn is_square_set(self, square: Square) -> Bool{
+       False
+    }
+}
+
+#[derive(Copy, Clone)]
+enum Color {
+    White,
+    Black,
+}
+
+#[derive(Copy, Clone)]
+enum Piece_Type {
+    nWhite,
+    nBlack,
+    nPawn,
+    nBishop,
+    nRook,
+    nQueen,
+    nKing,
+}
+
+pub struct Position {
+
+    pieceBB: [BB; 8],
+}
+
+impl Position {
+    
+    fn piece_code(&self, pt: Piece_Type) -> usize {
+        match pt {
+            Piece_Type::nPawn => Piece_Type::nPawn as usize,
+            Piece_Type::nBishop => Piece_Type::nBishop as usize,
+            Piece_Type::nRook => Piece_Type::nRook as usize,
+            Piece_Type::nQueen => Piece_Type::nQueen as usize,
+            Piece_Type::nKing => Piece_Type::nKing as usize,
+            _ => panic!("Invalid piece type"),
+        }
+    }
+
+    fn color_code(&self, color: Color){
+        match color {
+            Color::White => Piece_Type::nWhite as usize,
+            Color::Black => Piece_Type::nBlack as usize,
+            _ => panic!("Invalid color"),
+        }
+    }
+
+    fn get_piece_set(&self, pt: PieceType, cC: colorCode) -> BB {
+        self.pieceBB[Self::piece_code(pt)] & self.pieceBB[Self::color_code(cC)]
+    }
+
+    fn get_white_pawns(&self) -> BB {
+        self.pieceBB[Self::piece_code(Piece_Type::nPawn)] & self.pieceBB[Self::color_code(Color::White)]
+    }
+
+    fn get_black_pawns(&self) -> BB {
+        self.pieceBB[Self::piece_code(Piece_Type::nPawn)] & self.pieceBB[Self::color_code(Color::Black)]
+    }
+
+    fn get_pawns(&self, ct: Color) -> BB {
+        self.pieceBB[Self::piece_code(Piece_Type::nPawn)] & self.piece_bb[Self::color_code]
+    }
+}
+
---
+++ src/board/board_state.rs
@@ -0,0 +1,7 @@
+
+
+pub struct State {
+
+    castling: Castling,
+    
+}
