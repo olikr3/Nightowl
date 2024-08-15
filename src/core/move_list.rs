a58a6b98975ef487a73c106a2d3ff24cb885d695
olikr3 <oliver.kremser@gmail.com>
2024-08-15 10:14:19 +0200

    Add files via upload

---
+++ src/core/move_list.rs
@@ -0,0 +1,11 @@
+//generates all possible moves for a given piece in a given position
+
+pub struct Move_List {
+    mlist: Vec<Move> = vec!();
+}
+
+impl Move_List {
+    fn new() -> Self {
+
+    }
+}
