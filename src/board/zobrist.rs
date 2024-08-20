804d862c976b7eef45f32e5a28d3ac83433f1fae
pkep <oliver.kremser@proton.me>
2024-08-20 16:04:29 +0200

    started implementing zobrist

---
+++ src/board/zobrist.rs
@@ -0,0 +1,16 @@
+
+use rand::Rng;
+
+const PIECE_TYPES: usize = 12; // 6 for white, 6 for black
+const SQUARES: usize = 64;
+
+struct Zobrist {
+    piece_square_keys: [[u64; SQUARES]; PIECE_TYPES],
+    side_to_move_key: u64,
+    castling_rights_keys: [u64; 16], // 4 castling rights (KQkq)
+    en_passant_keys: [u64; 8], // 8 possible files for en passant
+}
+
+impl Zobrist {
+    
+}
