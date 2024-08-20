8535bd6da3e69cf6eed88f1251d166de18300acd
olikr3 <oliver.kremser@gmail.com>
2024-08-20 16:16:14 +0200

    constructor for zobrist

--- src/board/zobrist.rs
+++ src/board/zobrist.rs
@@ -1,4 +1,9 @@
 
+
+/// The idea of zobrist hashing is to generate a unique hash for each chess position.
+/// This hash is then used to determine whether a specific position has been encountered before.
+/// This is done by using XOR - operations on pieces, castling rights, etc. to create a unique identifier.
+
 use rand::Rng;
 
 const PIECE_TYPES: usize = 12; // 6 for white, 6 for black
@@ -12,5 +17,33 @@ struct Zobrist {
 }
 
 impl Zobrist {
-    fn new()
+    fn new() -> Self {
+        let mut rng = rand::thread_rng();
+        let mut piece_square_keys = [[0u64; SQUARES]; PIECE_TYPES];
+        let mut side_to_move_key = rng.gen::<u64>();
+        let mut castling_rights_keys = [0u64; 16];
+        let mut en_passant_keys = [0u64; 8];
+
+        for piece in 0..PIECE_TYPES {
+            for square in 0..SQUARES {
+                piece_square_keys[piece][square] = rng.gen::<u64>();
+            }
+        }
+
+        for i in 0..16 {
+            castling_rights_keys[i] = rng.gen::<u64>();
+        }
+
+        for i in 0..8 {
+            en_passant_keys[i] = rng.gen::<u64>();
+        }
+
+        Zobrist {
+            piece_square_keys,
+            side_to_move_key,
+            castling_rights_keys,
+            en_passant_keys,
+        }
+    }
+    
 }
