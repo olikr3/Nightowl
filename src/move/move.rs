e429b25d62f901f0efcb94f95b6dc2cf01ae1158
olikr3 <oliver.kremser@gmail.com>
2025-02-12 18:27:28 +0100

    added helper methods to detect flags

--- src/move/move.rs
+++ src/move/move.rs
@@ -1,6 +1,7 @@
 
+use super::*;
 
-/// represents a single move
+/// represents a single move, following the from-to based approach (see https://www.chessprogramming.org/Encoding_Moves)
 
 /// a Move should never be created manually - otherwise legality cannot be ensured.
 
@@ -79,6 +80,39 @@ impl MoveFlag {
     pub fn to_u8(self) -> u8 {
         self as u8
     }
+
+    /// Checks if this move is a capture.
+    pub fn is_capture(self) -> bool {
+        matches!(
+            self,
+            MoveFlag::Capture 
+            | MoveFlag::EnPassantCapture 
+            | MoveFlag::KnightPromoCapture 
+            | MoveFlag::BishopPromoCapture 
+            | MoveFlag::RookPromoCapture 
+            | MoveFlag::QueenPromoCapture
+        )
+    }
+
+    /// Checks if this move is a promotion.
+    pub fn is_promotion(self) -> bool {
+        matches!(
+            self,
+            MoveFlag::KnightPromotion 
+            | MoveFlag::BishopPromotion 
+            | MoveFlag::RookPromotion 
+            | MoveFlag::QueenPromotion 
+            | MoveFlag::KnightPromoCapture 
+            | MoveFlag::BishopPromoCapture 
+            | MoveFlag::RookPromoCapture 
+            | MoveFlag::QueenPromoCapture
+        )
+    }
+
+    /// Checks if this move is a castling move.
+    pub fn is_castling(self) -> bool {
+        matches!(self, MoveFlag::KingCastle | MoveFlag::QueenCastle)
+    }
 }
 
 impl Move {
@@ -104,5 +138,20 @@ impl Move {
     /// Extracts the move flag (bits 0-3)
     pub fn flag(self) -> Option<MoveFlag> {
         MoveFlag::from_u8((self.0 & 0b1111) as u8)
+    }
+
+    /// Checks if this move is a capture.
+    pub fn is_capture(self) -> bool {
+        self.flag().map_or(false, |flag| flag.is_capture())
+    }
+
+    /// Checks if this move is a promotion.
+    pub fn is_promotion(self) -> bool {
+        self.flag().map_or(false, |flag| flag.is_promotion())
+    }
+
+    /// Checks if this move is a castling move.
+    pub fn is_castling(self) -> bool {
+        self.flag().map_or(false, |flag| flag.is_castling())
     }
 }
