c446acc0668b159dcf1131dbef0e461992178df6
olikr3 <oliver.kremser@gmail.com>
2025-02-12 19:13:55 +0100

    basic layout

--- src/move/move_list.rs
+++ src/move/move_list.rs
@@ -1,11 +1,52 @@
-//generates all possible moves for a given piece in a given position
-
-pub struct Move_List {
-    mlist: Vec<Move> = vec!();
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct MoveList {
+    moves: [Move; 256], // Max possible legal moves should be lower than 256
+    count: usize,
 }
 
-impl Move_List {
-    fn new() -> Self {
+impl MoveList {
+    /// Creates empty MoveList
+    pub fn new() -> Self {
+        MoveList {
+            moves: [Move(0); 256],
+            count: 0,
+        }
+    }
 
+    /// Adds a move to the list
+    pub fn add(&mut self, mv: Move) {
+        if self.count < 256 {
+            self.moves[self.count] = mv;
+            self.count += 1;
+        }
+    }
+
+    /// Returns the number of moves in the list
+    pub fn len(&self) -> usize {
+        self.count
+    }
+
+    /// Checks if the list is empty
+    pub fn is_empty(&self) -> bool {
+        self.count == 0
+    }
+
+    /// Get move by index
+    pub fn get(&self, index: usize) -> Option<Move> {
+        if index < self.count {
+            Some(self.moves[index])
+        } else {
+            None
+        }
+    }
+
+    /// Clears the move list
+    pub fn clear(&mut self) {
+        self.count = 0;
+    }
+
+    /// Iterates over the stored moves
+    pub fn iter(&self) -> impl Iterator<Item = &Move> {
+        self.moves[..self.count].iter()
     }
 }
