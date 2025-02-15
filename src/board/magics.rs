d0d791531a05f54b07f30eb18fe8cfdc67744c7b
olikr3 <oliver.kremser@gmail.com>
2025-02-15 12:11:11 +0100

    added methods

--- src/board/magics.rs
+++ src/board/magics.rs
@@ -5,11 +5,26 @@ const ATTACK_TABLE_SIZE = 88507;
 
 static mut ATTACK_TABLE: [BB; ATTACK_TABLE_SIZE]
 
+static mut BISHOP_MAGIC: [BlackMagic; 64] = [BlackMagic {
+    ptr: &[],
+    notmask: 0,
+    black_magic: 0,
+}; 64];
+
+static mut ROOK_MAGIC: [BlackMagic; 64] = [BlackMagic {
+    ptr: &[],
+    notmask: 0,
+    black_magic: 0,
+}; 64];
 
 #[derive(Debug, Clone, Copy)]
 pub struct BlackMagic {
     ptr: &'static [BB], // reference to attack table slice
     notmask: BB; // for relevant occupancy bits
-    black_magic: bb,
+    black_magic: BB,
 }
 
+impl BlackMagic {}
+
+fn get_rook_attacks(occ: BB, square: usize) -> BB {}
+fn get_bishop_attacks(occ: BB, square: usize) -> BB {}
