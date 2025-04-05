9a8e2017656ef62a714d632aa36053140f8af56d
olikr3 <oliver.kremser@gmail.com>
2025-04-05 16:46:47 +0200

    added methods for generating moves

--- src/move/movegen.rs
+++ src/move/movegen.rs
@@ -1,17 +1,18 @@
 pub struct MoveGenerator;
 
 impl MoveGenerator {
+
     /// Generate all legal moves for the given board state.
     pub fn generate_moves(board: &Board, move_list: &mut MoveList) {
-        // Generate pseudo-legal moves (legal except for checks)
+
         MoveGenerator::generate_pseudo_legal_moves(board, move_list);
         
-        // Filter out illegal moves that leave the king in check
         MoveGenerator::filter_legal_moves(board, move_list);
     }
 
     /// Generates pseudo-legal moves (ignoring checks)
     fn generate_pseudo_legal_moves(board: &Board, move_list: &mut MoveList) {
+
         MoveGenerator::generate_pawn_moves(board, move_list);
         MoveGenerator::generate_knight_moves(board, move_list);
         MoveGenerator::generate_bishop_moves(board, move_list);
@@ -23,6 +24,7 @@ impl MoveGenerator {
 
     /// Filters out illegal moves that leave the king in check
     fn filter_legal_moves(board: &Board, move_list: &mut MoveList) {
+
         move_list.moves.retain(|&mv| {
             board.make_move(mv);
             let legal = !board.is_king_in_check(board.side_to_move());
@@ -30,4 +32,35 @@ impl MoveGenerator {
             legal
         });
     }
+
+    fn generate_pawn_moves(board: &Board, move_list: &mut MoveList) {
+    
+        todo!
+    }
+
+    fn generate_knight_moves(board: &Board, move_list: &mut MoveList) {
+        
+        todo!
+    }
+
+    fn generate_bishop_moves(board: &Board, move_list: &mut MoveList) {
+        
+        todo!
+    }
+
+    fn generate_queen_moves(board: &Board, move_list: &mut MoveList) {
+
+        todo!
+    }
+
+    fn generate_king_moves(board: &Board, move_list: &mut MoveList) {
+        
+        todo!
+    }
+
+    fn generate_castling_moves(board: &Board, move_list: &mut MoveList) {
+
+        todo!
+    }
+
 }
