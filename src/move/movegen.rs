c6b9bc0e20f91a83181102e9a6f388c72377e7ea
olikr3 <oliver.kremser@gmail.com>
2025-04-05 16:50:27 +0200

    clarifying comments

--- src/move/movegen.rs
+++ src/move/movegen.rs
@@ -3,6 +3,8 @@ pub struct MoveGenerator;
 impl MoveGenerator {
 
     /// Generate all legal moves for the given board state.
+    /// by generating all possible moves and then filtering out illegal ones
+
     pub fn generate_moves(board: &Board, move_list: &mut MoveList) {
 
         MoveGenerator::generate_pseudo_legal_moves(board, move_list);
@@ -10,7 +12,6 @@ impl MoveGenerator {
         MoveGenerator::filter_legal_moves(board, move_list);
     }
 
-    /// Generates pseudo-legal moves (ignoring checks)
     fn generate_pseudo_legal_moves(board: &Board, move_list: &mut MoveList) {
 
         MoveGenerator::generate_pawn_moves(board, move_list);
@@ -22,45 +23,44 @@ impl MoveGenerator {
         MoveGenerator::generate_castling_moves(board, move_list);
     }
 
-    /// Filters out illegal moves that leave the king in check
     fn filter_legal_moves(board: &Board, move_list: &mut MoveList) {
 
         move_list.moves.retain(|&mv| {
-            board.make_move(mv);
+            board.make_move(mv); // not yet implemented
             let legal = !board.is_king_in_check(board.side_to_move());
-            board.unmake_move(mv);
+            board.unmake_move(mv); // not yet implemented
             legal
         });
     }
 
     fn generate_pawn_moves(board: &Board, move_list: &mut MoveList) {
     
-        todo!
+        todo()!
     }
 
     fn generate_knight_moves(board: &Board, move_list: &mut MoveList) {
         
-        todo!
+        todo()!
     }
 
     fn generate_bishop_moves(board: &Board, move_list: &mut MoveList) {
         
-        todo!
+        todo()!
     }
 
     fn generate_queen_moves(board: &Board, move_list: &mut MoveList) {
 
-        todo!
+        todo()!
     }
 
     fn generate_king_moves(board: &Board, move_list: &mut MoveList) {
         
-        todo!
+        todo()!
     }
 
     fn generate_castling_moves(board: &Board, move_list: &mut MoveList) {
 
-        todo!
+        todo()!
     }
 
 }
