b7acc5421b605c6b4c84cc9afb7019eb755f6bac
olikr3 <oliver.kremser@gmail.com>
2025-04-06 14:53:48 +0200

    added build_board method

--- src/board/fen.rs
+++ src/board/fen.rs
@@ -18,4 +18,18 @@ impl FenParser {
     }
 
 
+    /// this functions assumes that the input fen string is valid
+    fn build_board(fen: &str) -> Board {
+
+        let parts = fen.split_whitespace().collect();
+        let pieces = parts[0].to_string(); // need to create the bbs for them
+        let active_color = parts[1].to_string();
+        let castling = todo!();
+        let en_passant = todo!();
+        let halfmove_clock = todo!();
+        let en_passant_target_square = todo()!;
+
+        Board {} //todo
+    }
+
 }
