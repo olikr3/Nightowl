95b7b3eb04fb0d4170e54acc553b03a40266cbcb
olikr3 <oliver.kremser@gmail.com>
2025-04-06 10:43:16 +0200

    added some logic

--- src/board/fen.rs
+++ src/board/fen.rs
@@ -1,4 +1,21 @@
 
-pub struct FenParser {
+
+/// for now mostly just pseudocode, as some details are still unclear
+pub struct FenParser {}
+
+impl FenParser {
+
+    /// creates a Board from a valid FEN-String
+    fn from_string(fen: &str) -> Result<Board, &'static str> {
+        
+        if !isvalid(fen) {
+            return Err("Invalid fen");
+        }
+        b: Board = Board {
+            todo!()
+        }
+        Ok(b)
+    }
+
 
 }
