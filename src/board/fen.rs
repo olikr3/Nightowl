ab778600f41c7211019d0d40546c1a20e7189519
olikr3 <oliver.kremser@gmail.com>
2025-04-06 15:04:59 +0200

    minor changes and comments

--- src/board/fen.rs
+++ src/board/fen.rs
@@ -21,15 +21,23 @@ impl FenParser {
     /// this functions assumes that the input fen string is valid
     fn build_board(fen: &str) -> Board {
 
+        // naming differences to board struct are atrocious for now
         let parts = fen.split_whitespace().collect();
         let pieces = parts[0].to_string(); // need to create the bbs for them
         let active_color = parts[1].to_string();
-        let castling = todo!();
-        let en_passant = todo!();
-        let halfmove_clock = todo!();
-        let en_passant_target_square = todo()!;
+        let castling = parts[2].to_string();
+        let en_passant_target_square = parts[3].to_string();
+        let halfmove_clock = parts[4].to_string();
+        let fullmove_number = parts[5].to_string();
 
-        Board {} //todo
+        Board {
+            pieceBB, //todo
+            side_to_move: active_color,
+            castling_rights: castling,
+            en_passant_square: en_passant_target_square,
+            // why do i need occupied and empty in board -> todo: go over that
+
+        } //todo
     }
 
 }
