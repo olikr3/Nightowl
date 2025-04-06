8041eeb2ebf7e92f00ddd5f99edb9b2fa52f0314
olikr3 <oliver.kremser@gmail.com>
2025-04-06 15:26:52 +0200

    added method heads

--- src/board/fen.rs
+++ src/board/fen.rs
@@ -19,7 +19,8 @@ impl FenParser {
 
 
     /// this functions assumes that the input fen string is valid
-    fn build_board(fen: &str) -> Board {
+    /// it might make more senst to write an iterator to process the string
+    fn build_board(fen: &'static str) -> Board {
 
         // naming differences to board struct are atrocious for now
         let parts = fen.split_whitespace().collect();
@@ -30,14 +31,29 @@ impl FenParser {
         let halfmove_clock = parts[4].to_string();
         let fullmove_number = parts[5].to_string();
 
+        // todo: match all strs to correct format
         Board {
-            pieceBB, //todo
-            side_to_move: active_color,
+            pieceBB, //todo: write method for that
+            side_to_move: active_color, // todo: match str to color
             castling_rights: castling,
             en_passant_square: en_passant_target_square,
             // why do i need occupied and empty in board -> todo: go over that
+            half_moves: halfmove_clock,
+            full_moves: fullmove_number,
 
         } //todo
     }
 
+    // question to self: should this even be in parser?
+    /// build all required biboards for the board struct
+    fn build_bbs(&'static str pieces) -> [pieceBB] {
+        todo!();
+    }
+
+    fn to_color(&'static str pieces) -> Color {
+        
+        todo!();
+    }
+
 }
+
