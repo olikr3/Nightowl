f1891e834bae1471673cc40c0464716b0e6a37c4
olikr3 <oliver.kremser@gmail.com>
2025-04-07 20:17:56 +0200

    implemented search method

--- src/engine/pvsearch.rs
+++ src/engine/pvsearch.rs
@@ -13,11 +13,11 @@ struct TranspositionEntry {
     zobrist_key: u64,
     depth: i32,
     value: i32,
-    flag: HashFlag,
+    flag: ScopeFlag,
 }
 
 #[derive(Clone, Copy, PartialEq)]
-enum Flag {
+enum ScopeFlag {
     Exact,
     Alpha,
     Beta,
@@ -32,7 +32,33 @@ impl PVSearch {
         }
     }
 
-    pub fn search(&mut self, board:&mut Board, depth: i32) -> Option<Move> {
-        todo!()
+    pub fn search(&mut self, board: &mut Board, max_depth: i32) -> Option<Move> {
+        let mut best_move = None;
+        let mut alpha = -CHECKMATE;
+        let beta = CHECKMATE;
+
+        for depth in 1..=max_depth {
+            let mut move_list = MoveList::new();
+            MoveGenerator::generate_moves(board, &mut move_list);
+            self.order_moves(&mut move_list, board); // not implemented
+
+            let mut best_score = -CHECKMATE;
+            for mv in move_list.moves.iter() {
+                board.make_move(*mv);
+                let score = -self.pv_search(board, -beta, -alpha, depth - 1);
+                board.unmake_move(*mv);
+
+                if score > best_score {
+                    best_score = score;
+                    best_move = Some(*mv);
+                }
+
+                if score > alpha {
+                    alpha = score;
+                }
+            }
+        }
+
+        best_move
     } 
 }
